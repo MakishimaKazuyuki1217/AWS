4ef5f50affd4f1c7ca345773bd9d6cf99480e898
MakishimaKazuyuki1217 <71403936+MakishimaKazuyuki1217@users.noreply.github.com>
2023-01-09 17:41:29 +0900

    Uncaught Error Error: Minified React error #423; visit https://reactjs.org/docs/error-decoder.html?invariant=423 for the full message or use the non-minified dev environment for full errors and additional helpful warnings.

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -1,15 +1,19 @@
 // 型のために導入
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage, NextPageContext } from 'next'
 // Next.jsの組み込みコンポーネント
 import Head from 'next/head'
+import { isPropertySignature } from 'typescript'
 
-// ページコンポーネントのpropsの型定義（ここでは空）
-type SSGProps = {}
+// ページコンポーネントのpropsの型宣言
+type SSGProps = {
+    message: string
+}
 
-// SSG向けのページを実装
-// NextPageはnext.jsのpages向けの方
-// NestPage<props>でpropsが入るPageであることを明示
-const SSG: NextPage<SSGProps> = () => {
+// SSGはgetStaticPropsがかえしたprops受け取ることができる
+// NextPage<SSGProps>message: stringのみを受け取って生成されるページの型
+const SSG: NextPage<SSGProps> = (props) => {
+    const { message } = props
+
     return (
         <div>
             {   /* Headコンポーネントで包むと、その要素は<head>タグに配置されます　*/   }
@@ -19,11 +23,28 @@ const SSG: NextPage<SSGProps> = () => {
             </head>
             <main>
             <p>
-                このページは静的サイト生成に酔ってビルド時に生成されたページです
+                このページは静的サイト生成によってビルド時に生成されたページです
             </p>
             </main>
         </div>
     )
+}
+
+
+// getStaticPropsはビルド時に実行される
+// GetStaticProps<SSGProps>はSSGPropsを引数にとるgetStaticPropsの型
+export const getStaticProps: GetStaticProps<SSGProps> = async(context) => {
+    const timestamp = new Date().toLocaleString();
+    const message =  `${timestamp} にgetStaticPropsが実行されました`
+    console.log("----------------------------------------------")
+    console.log(message)
+
+    return {
+        // ここで返したpropsを元にページコンポーネントを描画する
+        props: {
+            message,
+        },
+    }
 }
 
 export default SSG
