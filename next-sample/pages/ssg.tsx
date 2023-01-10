80b30d0a4fd412f0a692c25fc9bee53eef5eea05
MakishimaKazuyuki1217 <71403936+MakishimaKazuyuki1217@users.noreply.github.com>
2023-01-10 14:05:55 +0900

    P.124までコーディング

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -8,7 +8,6 @@ type SSGProps = {
     message: string;
 };
 
-
 // SSGはgetStaticPropsが返したpropsを受け取ることができる
 // NextPage<SSGProps>はmessage: stringのみを受け取って生成されるページの型
 // Next.jsのページコンポーネントや関数の型は https://nextjs.org/learn/excel/typescript/nextjs-types もご参照ください
