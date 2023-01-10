e8860cc6026c574c44b3bc6e4f6efee91eb75e6d
MakishimaKazuyuki1217 <71403936+MakishimaKazuyuki1217@users.noreply.github.com>
2023-01-10 12:43:47 +0900

    getStaticPropsで生成時の時刻を表示

--- next-sample/pages/ssg.tsx
+++ next-sample/pages/ssg.tsx
@@ -1,13 +1,20 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage, NextPageContext } from 'next'
 import Head from 'next/head'
+import { isPropertySignature } from 'typescript';
+import { isContext } from 'vm';
 
 // ページコンポーネントのpropsの型定義
-type SSGProps = {};
+type SSGProps = {
+    message: string;
+};
+
 
 // SSGはgetStaticPropsが返したpropsを受け取ることができる
 // NextPage<SSGProps>はmessage: stringのみを受け取って生成されるページの型
 // Next.jsのページコンポーネントや関数の型は https://nextjs.org/learn/excel/typescript/nextjs-types もご参照ください
-const SSG: NextPage<SSGProps> = () => {
+const SSG: NextPage<SSGProps> = (props) => {
+    const {message} = props;
+
     return (
         <div>
             <Head>
@@ -19,9 +26,24 @@ const SSG: NextPage<SSGProps> = () => {
                 <p>
                     このページは静的サイト生成によってビルド時に生成されたページです
                 </p>
+                <p>{message}</p>
             </main>
         </div>
   );
 };
+
+
+export const getStaticProps: GetStaticProps<SSGProps> = async(context) => {
+    const timestamp = new Date().toLocaleString()
+    const message = `${timestamp} にgetStaticPropsが実行されました`
+
+    console.log(message)
+
+    return {
+        props: {
+            message,
+        }
+    }
+}
 
 export default SSG;
