d9439c17ba4c7a6b1519b197909a92e3a433450e
MakishimaKazuyuki1217 <71403936+MakishimaKazuyuki1217@users.noreply.github.com>
2023-01-20 13:35:44 +0900

    p.167 LinkToを使用したストーリー間の遷移

--- next-sample/stories/StyledButton.stories.tsx
+++ next-sample/stories/StyledButton.stories.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
 import { ComponentMeta, ComponentStory, Story } from '@storybook/react'
 import { StyledButton, StyledButtonProps } from '../components/StyledButton'
-// 新しくactionをインポート
-import { action } from '@storybook/addon-actions'
-
-import MDXDocument from '../stories/StyledButton.mdx'
-
+import { linkTo } from '@storybook/addon-links'
 
 
 
@@ -16,95 +12,28 @@ export default {
     // 使用するコンポーネント
     component: StyledButton,
 
-    // 以下の行を追加（また、著者がぶっこんできたｗｗｗ
-//    argTypes: { onClick: { action: 'clicked' } },
-    parameters: {
-        docs: {
-            page: MDXDocument,
-        },
-    },
-
-/*    
-argTypes: {
-        variant: {
-            control: {type: 'radio' },
-            options: [ 'primary', 'success', 'transparent' ]
-        },
-        children: {
-            control: {type: 'text'}, 
-        },
-    },
-*/
-
 } as ComponentMeta<typeof StyledButton>
 
-
-const Template: ComponentStory<typeof StyledButton> = (args) => <StyledButton { ...args} />
-
-export const TemplateTest = Template.bind({})
-
-// デフォルトのPropsを設定する
-TemplateTest.args = {
-    variant: 'primary',
-    children: 'Primary',
-}
-
-
-// increment という名前でactionを出力するための関数を作る
-const incrementAction = action('increment')  
-
-function MyFunction(arg: string)
-{
-    console.log(arg)
-
-    return arg
-}
-
-
-
 export const Primary = (props : any)  => {
-    const [count , setCount] = useState(0)
-    const onClick = (e: React.MouseEvent) => {
-
-        incrementAction(e, count)
-
-        MyFunction("前の会社から株主総会の案内きた～")
-
-        setCount(c => c + 2)
-    }
-
     return (
-        <StyledButton {...props} variant="primary" onClick={onClick}>
-            Count: {count}
-                 <audio id="btn_audio">
-                    
-                </audio>
-        </StyledButton>
-    )
-}
-
-/*
-export const Primary: Story<StyledButtonProps> = (props) => {
-    return (
-        <StyledButton {...props} variant ="primary">
+        <StyledButton {...props} variant="primary" onClick={linkTo('StyledButton', 'Success')}>
         Primary
-    </StyledButton>
-    )
-}
-*/
-
-export const Success: Story<StyledButtonProps> = (props) => { 
-    return (
-        <StyledButton {...props} variant ="success">
-            Primary
         </StyledButton>
     )
 }
 
-export const Transparent: Story<StyledButtonProps> = (props) => {
+export const Success = (props : any) => { 
     return (
-        <StyledButton {...props} variant="transparent">
-            Transparent
+        <StyledButton {...props} variant="primary" onClick={linkTo('StyledButton', 'Transparent')}>
+        Primary
+        </StyledButton>
+    )
+}
+
+export const Transparent = (props : any) => { 
+    return (
+        <StyledButton {...props} variant="primary" onClick={linkTo('StyledButton', 'Primary')}>
+        Transparent
         </StyledButton>
     )
 }
